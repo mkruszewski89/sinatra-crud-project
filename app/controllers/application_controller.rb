998926cf696857e64dea37cbc895113afe6138dc
github username <github email address>
2018-03-13 21:03:29 -0400

    login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,12 +14,29 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     user = User.find_by(email: params[:email])
     if user
-      flash[:message] = "Email already in use!"
+      flash[:message] = "Email already in use"
+      redirect "/"
+    elsif params[:email] == "" || params[:password] == ""
+      flash[:message] = "Please enter an email and password"
       redirect "/"
     else
       user = User.create(params)
       session[:id] = user.id
       redirect "/account"
+    end
+  end
+
+  post "/login" do
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:id] = user.id
+      redirect "/account"
+    elsif params[:email] == "" || params[:password] == ""
+      flash[:message] = "Please enter an email and password"
+      redirect "/"
+    else
+      flash[:message] = "Email and password combination not found"
+      redirect "/"
     end
   end
 
